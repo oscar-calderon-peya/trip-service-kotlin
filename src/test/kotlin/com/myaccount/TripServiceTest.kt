33f5c1d80837cb1cb8b4e95480218aea726ed654
oscar-calderon-peya <65566222+oscar-calderon-peya@users.noreply.github.com>
2020-08-03 13:22:13 -0500

    Initial commit

---
+++ src/test/kotlin/com/myaccount/TripServiceTest.kt
@@ -0,0 +1,12 @@
+package com.myaccount
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.Test
+
+class TripServiceTest {
+
+    @Test
+    fun `failed test`() {
+        then(false).isEqualTo(true)
+    }
+}
